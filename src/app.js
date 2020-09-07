fc4277643026d7c44b67df18e4132574466843b2
MrPrettyPotato <56453053+MrPrettyPotato@users.noreply.github.com>
2020-09-07 15:12:29 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+const data = (title,name,info)=>{
+    const data = {
+        title,
+        name,
+        info
+    }
+    return data
+}
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', data('Home page', 'Kristof Volkaerts'))
+})
+app.get('/about', (req, res) => {
+    res.render('about', data('About Me', 'Kristof Volkaerts'))
+})
+app.get('/help', (req, res) => {
+    res.render('help', data('Help page', 'Kristof Volkaerts'))
+})
+
+
+
+
+app.get('/weather', (req, res) => {
+    
+    if(!req.query.adress) {
+        return res.send({
+            error: 'You must provide a location'
+        })
+    }
+    geocode(req.query.adress, (error, {latitude,longtitude,location} = {})=>{
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            return res.send(forecastData)
+        })
+    })
+    
+})
+
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search) {
+        return res.send({
+            error:'You must provide a search term'
+        })
+    } 
+    console.log(req.query)
+    res.send({
+        products:[]
+    })
+
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404', data('404 page', 'Kristof Volkaerts','Help page not found!'))
+})
+app.get('/about/*',(req,res)=>{
+    res.render('404', data('404 page', 'Kristof Volkaerts','About page not found!'))
+})
+app.get('*', (req,res) =>{
+    res.render('404', data('404 page', 'Kristof Volkaerts'))
+    })
+//app.com
+//app.com/help
+//app.com/about
+
+// Starts the app
+app.listen(3000, () => {
+    console.log('Server started!')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,34 @@
+const request = require('request')
+
+const forecast = (long,lat,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7ccddbf609f18952dde89b5f8d44564f&query='+lat+','+long
+
+    request({url,json:true},(error,{body}) =>{
+        if(error) {
+            callback('Canot connect to the location service! '+ error,undefined)
+        } else if(body.error) {
+            callback('Unable to use these coords!' + response.body,undefined)
+        } else {
+            
+            const {name,country,region,lat,lon} = body.location
+            const {temperature,wind_speed:windSpeed,feelslike} = body.current
+             callback(undefined,{
+                 info: 'The temperature is :' + temperature + '° and it feels like '+ feelslike + ' with a windspeed of ' + windSpeed,
+                 location: name,
+                 country,
+                 temperature,
+                 windSpeed
+             })
+        }
+    })
+}
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+module.exports = forecast
